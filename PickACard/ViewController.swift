27592e8bc002776e1bffabc89b4d27d8e686d96a
MapleInSpring <hyf94244280@gmail.com>
2016-04-05 08:42:23 +0800

    Initial Commit

---
+++ PickACard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickACard
+//
+//  Created by Yifeng Hou on 5/4/16.
+//  Copyright © 2016 GraceHou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
